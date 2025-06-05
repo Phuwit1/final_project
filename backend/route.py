352afcdd63709e091be19e15e8a35ee4b3bbeaad
Phuwit1 <65070184@kmitl.ac.th>
2025-06-05 15:55:32 +0700

    add model request to route.py

--- backend/route.py
+++ backend/route.py
@@ -2,18 +2,23 @@ from fastapi import FastAPI
 import requests
 import os
 from dotenv import load_dotenv, dotenv_values
+from pydantic import BaseModel
+
 load_dotenv()
 
 app = FastAPI()
 
+class RouteRequest(BaseModel):
+    start: str
+    goal: str
+    start_time: str
 
-@app.get("/route/")
-async def root():
+@app.get("/route")
+async def root(text: RouteRequest):
     url = "https://navitime-route-totalnavi.p.rapidapi.com/route_transit"
     rapidapi_key = os.getenv("RAPIDAPI_KEY")
 
-    querystring = {"start":"35.665251,139.712092","goal":"35.661971,139.703795","datum":"wgs84","term":"1440","limit":"5","start_time":"2025-06-04T10:00:00","coord_unit":"degree"}
-    querystring = {"start":"35.665251,139.712092","goal":"35.661971,139.703795","datum":"wgs84","term":"1440","limit":"5","start_time":"2025-06-04T10:00:00","coord_unit":"degree"}
+    querystring = {"start": text.start,"goal": text.goal,"datum":"wgs84","term":"1440","limit":"5","start_time": text.start_time,"coord_unit":"degree"}
 
     headers = {
         "x-rapidapi-key": rapidapi_key,
